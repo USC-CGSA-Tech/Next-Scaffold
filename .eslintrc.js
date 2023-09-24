2e56140ae057676bf9f835835cbb6c5d1983a0cc
Haozhe Zhang <zhz19980514@gmail.com>
2023-09-24 15:27:26 -0500

    feat: added new eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  ignorePatterns: [
+    "dist",
+    "node_modules/*"
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    'react-refresh',
+    "@typescript-eslint"
+  ],
+  extends: [
+    "next/core-web-vitals",
+    "react",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb",
+    "prettier",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "plugin:security/recommended",
+  ],
+  overrides: [
+    {
+      files: [
+        "**/*.ts",
+        "**/*.tsx"
+      ],
+      parser: "@typescript-eslint/parser",
+      settings: {
+        react: {
+          version: "detect"
+        }
+      },
+      extends: [
+        "plugin:react/recommended",
+        "plugin:prettier/recommended"
+      ],
+      rules: {
+        "no-console": "error",
+        "@typescript-eslint/no-unused-vars": "error",
+      }
+    }
+  ],
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 export default function Home() {
-  console.log(222)
-  return (
-    <div className='2222'></div>
-  )
+  const d = 1;
+  console.log(1111);
+  return <div className='2222'></div>;
 }
